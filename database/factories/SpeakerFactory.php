7192a99843e354a011bb96cc830f2acb1c39c7b8
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2025-06-13 19:04:15 +0700

    feat: setup db

---
+++ database/factories/SpeakerFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Event;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Speaker>
+ */
+class SpeakerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name,
+            'email' => fake()->unique()->safeEmail,
+            'bio' => fake()->paragraph,
+            'photo' => 'https://randomuser.me/api/portraits/'
+                . fake()->randomElement(['women', 'men']) . '/'
+                . fake()->randomDigitNotZero() . '.jpg',
+            'twitter' => fake()->userName(),
+            'city' => fake()->city,
+            'country' => fake()->country,
+            'event_id' => Event::query()->pluck('id')->random()
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Speaker;
+use App\Models\Event;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->create();
+         $events = Event::factory(10)->create();
+
+         $events->each(function ($event) {
+            $event->speakers()->saveMany(Speaker::factory(3)->make());
+         });
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
